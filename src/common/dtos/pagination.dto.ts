1ee016d4e9d6e59efe8aa16969d42014cdb5deb6
starlin de la cruz <starlinr82@gmail.com>
2023-05-24 17:20:39 -0400

    first commit

---
+++ src/common/dtos/pagination.dto.ts
@@ -0,0 +1,16 @@
+import { Type } from "class-transformer"
+import { IsOptional, IsPositive, Min } from "class-validator"
+
+export class PaginationDto {
+    @IsOptional()
+    @IsPositive()
+    // transformar
+    @Type(() => Number)
+    limit?: number
+    
+    @IsOptional()
+    @IsPositive()
+    @Type(() => Number)
+    @Min(0)
+    offset?: number
+}
---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,46 @@
+import {
+    IsString,
+    MinLength,
+    IsPositive,
+    IsOptional,
+    IsNumber,
+    IsInt,
+    IsArray,
+    IsIn
+} from "class-validator"
+
+
+export class CreateProductDto {
+    @IsString()
+    @MinLength(1)
+    title: string
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?: number
+    
+    @IsString()
+    @IsOptional()
+    description?: string
+    
+    @IsString()
+    @IsOptional()
+    slug?: string
+    
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    stock?: number
+    
+    @IsString({each: true})
+    @IsArray()
+    sizes: string[]
+
+    @IsString({each: true})
+    @IsArray()
+    tags: string[]
+    
+    @IsIn(['men','woman','kid','unisex'])
+    gender: string
+}
